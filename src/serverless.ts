a9a80f2cdb96cbb1544089aab4c99cd4d0bd422f
faizmamutov <faiz.mamutov@ergonized.com>
2022-11-23 21:21:22 +0600

    fix serverless file

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,5 +1,6 @@
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 
 import { AppModule } from './app.module';
@@ -8,6 +9,7 @@ let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
