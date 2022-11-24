8e88e726c7349b6b45ed5513e001e97004427175
faizmamutov <faiz.mamutov@ergonized.com>
2022-11-24 15:41:41 +0600

    add headers

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
   await app.listen(3002);
 }
 bootstrap();
